f1859ab953b25c401ed52e9ecf19e0d29ea15015
MuhammadFikrri <muhammadfikrri7@gmail.com>
2023-01-18 17:13:44 +0700

    fix template 1

---
+++ src/test/java/starter/JsonPlaceHolder/TodoAPI.java
@@ -0,0 +1,28 @@
+package starter.JsonPlaceHolder;
+
+import io.restassured.http.ContentType;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Step;
+import starter.JsonPlaceHolder.Utils.Constant;
+
+import java.io.File;
+
+public class TodoAPI
+{
+    public static String GET_TODOS = Constant.BASE_URL + "/todos?id={id}";
+    public static String POST_CREATE_TODOS = Constant.BASE_URL + "/todos";
+
+    @Step("Get list todos")
+    public void setGetListTodos(int id)
+    {
+        SerenityRest.given().pathParam("id", id);
+    }
+
+    @Step("Post create todos")
+    public void setPostCreateTodos(File json)
+    {
+        SerenityRest.given()
+                .contentType(ContentType.JSON)
+                .body(json);
+    }
+}
---
+++ src/test/java/starter/JsonPlaceHolder/Utils/Constant.java
@@ -0,0 +1,19 @@
+package starter.JsonPlaceHolder.Utils;
+
+public class Constant
+{
+    public static String BASE_URL = "https://jsonplaceholder.typicode.com";
+    public static final String DIR = System.getProperty("user.dir");
+
+    public static String TODO_JSON_REQUEST = DIR+"/src/test/resources/JSON/Request/Todos/(Nama File)";
+    public static String POSTS_JSON_REQUEST = DIR+"/src/test/resources/JSON/Request/Posts/(Nama File)";
+    public static String COMMENTS_JSON_REQUEST = DIR+"/src/test/resources/JSON/Request/Comments/(Nama File)";
+    public static String ALBUMS_JSON_REQUEST = DIR+"/src/test/resources/JSON/Request/Albums/(Nama File)";
+    public static String PHOTOS_JSON_REQUEST = DIR+"/src/test/resources/JSON/Request/Photos/(Nama File)";
+
+    public static String TODO_JSON_SCHEMA = DIR+"/src/test/resources/JSON/JsonSchema/Todos/(Nama File)";
+    public static String POSTS_JSON_SCHEMA = DIR+"/src/test/resources/JSON/JsonSchema/Posts/(Nama File)";
+    public static String COMMENTS_JSON_SCHEMA = DIR+"/src/test/resources/JSON/JsonSchema/Comments/(Nama File)";
+    public static String ALBUMS_JSON_SCHEMA = DIR+"/src/test/resources/JSON/JsonSchema/Albums/(Nama File)";
+    public static String PHOTOS_JSON_SCHEMA = DIR+"/src/test/resources/JSON/JsonSchema/Photos/(Nama File)";
+}
